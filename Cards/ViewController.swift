40ea86563ae3cb751cb4d59fc5f2eb8e863143e2
Vitaly Glushkov <vit8@live.ru>
2022-08-15 15:57:03 +0400

    Initial Commit

---
+++ Cards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cards
+//
+//  Created by Vitaly Glushkov on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
